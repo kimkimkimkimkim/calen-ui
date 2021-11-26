2d9a5f57fdfcf21969a034362a1bd447f7a7e020
kim sangwoo <kim1219sangwoo@yahoo.co.jp>
2021-11-26 10:47:01 +0900

    Create index.js

---
+++ scripts/screens/index.js
@@ -0,0 +1,5 @@
+export { default as HomeScreen } from "./screen/HomeScreen"
+export { default as FriendScreen } from "./screen/FriendScreen"
+export { default as NotificationScreen } from "./screen/NotificationScreen"
+export { default as ChatScreen } from "./screen/ChatScreen"
+export { default as TimelineScreen } from "./screen/TimelineScreen"
